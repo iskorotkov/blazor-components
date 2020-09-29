5c1e78518093926db13cd15a910da68cd50ba2a1
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-09-29 19:01:16 +0500

    Use struct System.Drawing.Color instead of custom class

--- src/InputComponents/Colors/ColorVm.cs
+++ src/InputComponents/Colors/ColorVm.cs
@@ -1,14 +1,20 @@
-﻿namespace InputComponents.Colors
+﻿using System.Drawing;
+
+namespace InputComponents.Colors
 {
-    public class Color
+    public class ColorVm
     {
-        public Color(int r, int g, int b, int a)
+        public ColorVm(int r, int g, int b, int a)
         {
             R = r;
             G = g;
             B = b;
             A = a;
         }
+
+        public Color ToColor() => Color.FromArgb(A, R, G, B);
+
+        public void FromColor(Color color) => (R, G, B, A) = (color.R, color.G, color.B, color.A);
 
         public int R { get; set; }
         public int G { get; set; }
