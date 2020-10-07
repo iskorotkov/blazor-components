170f133a154a67b149bbd0650ad3fff6367c4b45
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-10-07 23:26:18 +0500

    Add documentation

--- src/InputComponents/Colors/ColorPickerMode.cs
+++ src/InputComponents/Colors/ColorPickerMode.cs
@@ -2,21 +2,49 @@
 
 namespace InputComponents.Colors
 {
+    /// <summary>
+    /// Enumeration of supported <c>ColorPicker</c> modes.
+    /// </summary>
     public enum ColorPickerMode
     {
+        /// <summary>
+        /// RGBA format with base 10 numbers.
+        /// </summary>
         [FriendlyName("RGBA (dec)")] RgbaDec,
+        /// <summary>
+        /// RGBA format with base 16 numbers.
+        /// </summary>
         [FriendlyName("RGBA (hex)")] RgbaHex
     }
 
+    /// <summary>
+    /// An attribute that adds a user-friendly string to an enum value.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Field)]
     public class FriendlyNameAttribute : Attribute
     {
+        /// <summary>
+        /// Add specified user-friendly name to enum value.
+        /// </summary>
+        /// <param name="name">User-friendly string to show in UI.</param>
         public FriendlyNameAttribute(string name) => Name = name;
+
+        /// <summary>
+        /// User-friendly name of enum value.
+        /// </summary>
         public string Name { get; }
     }
 
+    /// <summary>
+    /// Helpers for <c>FriendlyNameAttribute</c>.
+    /// </summary>
     public static class EnumExtensions
     {
+        /// <summary>
+        /// Get user-friendly name of given enum value.
+        /// </summary>
+        /// <param name="value">Value of enum type with <c>FriendlyNameAttribute</c> on it.</param>
+        /// <returns>User-friendly name.</returns>
         public static string FriendlyName(this Enum value)
         {
             var type = value.GetType();
--- src/InputComponents/Colors/ColorVm.cs
+++ src/InputComponents/Colors/ColorVm.cs
@@ -2,8 +2,17 @@
 
 namespace InputComponents.Colors
 {
+    /// <summary>
+    /// ViewModel for <c>ColorPicker</c> component.
+    /// </summary>
     public class ColorVm
     {
+        /// <summary>
+        /// Create <c>ColorVm</c> with provided <paramref name="r"/> <paramref name="g"/> <paramref name="b"/> <paramref name="a"/> values.
+        /// </summary>
+        /// <remarks>
+        /// <para>Values should be between 0 and 255 (inclusive).</para>
+        /// </remarks>
         public ColorVm(int r, int g, int b, int a)
         {
             R = r;
@@ -12,15 +21,43 @@ namespace InputComponents.Colors
             A = a;
         }
 
+        /// <summary>
+        /// Convert to <c>System.Drawing.Color</c> struct.
+        /// </summary>
         public Color ToColor() => Color.FromArgb(A, R, G, B);
 
+        /// <summary>
+        /// Create <c>ColorVm</c> with values from <c>System.Drawing.Color</c>.
+        /// </summary>
         public void FromColor(Color color) => (R, G, B, A) = (color.R, color.G, color.B, color.A);
 
+        /// <summary>
+        /// Red value (between 0 and 255 inclusively).
+        /// </summary>
         public int R { get; set; }
+
+        /// <summary>
+        /// Green value (between 0 and 255 inclusive).
+        /// </summary>
         public int G { get; set; }
+
+        /// <summary>
+        /// Blue value (between 0 and 255 inclusive).
+        /// </summary>
         public int B { get; set; }
+
+        /// <summary>
+        /// Alpha value (between 0 and 255 inclusive).
+        /// </summary>
         public int A { get; set; }
 
+        /// <summary>
+        /// Color string for using with CSS attribute.
+        /// </summary>
+        /// <remarks>
+        /// <para><c>R</c>, <c>G</c>, <c>B</c> values are between 0 and 255 (inclusive), <c>A</c> is between 0 and 1.</para>
+        /// </remarks>
+        /// <value>"rgba({R}, {G}, {B}, {A / 255.0})"</value>
         public string CssValue => $"rgba({R}, {G}, {B}, {A / 255.0})";
     }
 }
